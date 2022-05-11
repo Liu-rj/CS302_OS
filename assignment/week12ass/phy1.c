f14aadec46a4a1ce02f677a4c52e521f3dd622b2
Liu-rj <11911808@mail.sustech.edu.cn>
2022-05-11 21:14:27 +0800

    update week12 assignment

--- assignment/week12ass/phy1.c
+++ assignment/week12ass/phy1.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <assert.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 #define N 200
 
@@ -11,19 +14,27 @@ pthread_mutex_t forks[5];
 
 pthread_mutex_t printer_lock;
 
-void update_bar(int percent_done) {
+void update_bar(int percent_done)
+{
     pthread_mutex_lock(&printer_lock);
-    if (percent_done < 1) {
+    if (percent_done < 1)
+    {
         pthread_mutex_unlock(&printer_lock);
         return;
     }
     printf("\r[");
-    for (int i = 0; i < 100; ++i) {
-        if (i < percent_done) {
+    for (int i = 0; i < 100; ++i)
+    {
+        if (i < percent_done)
+        {
             printf("-");
-        } else if (i == percent_done) {
+        }
+        else if (i == percent_done)
+        {
             printf(">");
-        } else {
+        }
+        else
+        {
             printf(" ");
         }
     }
@@ -32,93 +43,108 @@ void update_bar(int percent_done) {
     pthread_mutex_unlock(&printer_lock);
 }
 
-
 int eat_cnt = 0;
 int pick_cnt = 0;
 
-void pick_fork(int fork_no) {
+void pick_fork(int fork_no)
+{
     pthread_mutex_lock(&forks[fork_no]);
     __sync_fetch_and_add(&pick_cnt, 1);
 }
 
-void put_fork(int fork_no) {
+void put_fork(int fork_no)
+{
     pthread_mutex_unlock(&forks[fork_no]);
 }
 
-int left_fork_no(int p_no) {
+int left_fork_no(int p_no)
+{
     return p_no;
 }
 
-int right_fork_no(int p_no) {
+int right_fork_no(int p_no)
+{
     return (p_no + 1) % 5;
 }
 
-void pick_left_fork(int p_no) {
+void pick_left_fork(int p_no)
+{
     pick_fork(left_fork_no(p_no));
 }
 
-void pick_right_fork(int p_no) {
+void pick_right_fork(int p_no)
+{
     pick_fork(right_fork_no(p_no));
 }
 
-void put_left_fork(int p_no) {
+void put_left_fork(int p_no)
+{
     put_fork(left_fork_no(p_no));
 }
 
-void put_right_fork(int p_no) {
+void put_right_fork(int p_no)
+{
     put_fork(right_fork_no(p_no));
 }
 
-void eat(int p_no) {
+void eat(int p_no)
+{
     __sync_fetch_and_add(&eat_cnt, 1);
     update_bar(eat_cnt * 100 / (N * 5));
     usleep(1000);
 }
 //-----------------start----------------------
 // you can only modify this part
+pthread_mutex_t mutex;
 
-void init() {
-    // write code if you desire.
-
+void init()
+{
+    pthread_mutex_init(&mutex, NULL);
 }
 
-void wants_to_eat(int p_no) {
-    // fixme
-    pick_right_fork(p_no);
+void wants_to_eat(int p_no)
+{
+    pthread_mutex_lock(&mutex);
     pick_left_fork(p_no);
+    pick_right_fork(p_no);
 
     eat(p_no);
 
     put_left_fork(p_no);
     put_right_fork(p_no);
-
+    pthread_mutex_unlock(&mutex);
 }
 
 //----------------------end---------------------
 
-void *eat_many_times(void *arg) {
-    int p_no = *(int *) arg;
-    for (int i = 0; i < N; ++i) {
+void *eat_many_times(void *arg)
+{
+    int p_no = *(int *)arg;
+    for (int i = 0; i < N; ++i)
+    {
         wants_to_eat(p_no);
     }
     return NULL;
 }
 
-
-int main() {
+int main()
+{
     init();
     pthread_mutex_init(&printer_lock, NULL);
-    for (int i = 0; i < 5; ++i) {
+    for (int i = 0; i < 5; ++i)
+    {
         pthread_mutex_init(&forks[i], NULL);
-    } 
-
-    for (int i = 0; i < 5; ++i) {
-        int *no = malloc(sizeof(int));
-        *no = i;
-        pthread_create(&philosophers[i], NULL, eat_many_times, (void *) no);
     }
 
-    for (int i = 0; i < N; ++i) {
+    for (int i = 0; i < 5; ++i)
+    {
+        int *no = malloc(sizeof(int));
+        *no = i;
+        pthread_create(&philosophers[i], NULL, eat_many_times, (void *)no);
+    }
+
+    for (int i = 0; i < N; ++i)
+    {
         pthread_join(philosophers[i], NULL);
     }
 
--- assignment/week12ass/report.md
+++ assignment/week12ass/report.md
@@ -61,7 +61,20 @@ All processes are done, the system is not in a deadlock state.
 ## 2. Dining philosophers problem
 
 * The first solution is that we just let one philosopher to eat at one time, so there are no cyclic use of resources.
+
+![Screenshot from 2022-05-11 17-59-25](/home/lrj11911808/Pictures/Screenshot from 2022-05-11 17-59-25.png)
+
+![Screenshot from 2022-05-11 19-32-31](/home/lrj11911808/Pictures/Screenshot from 2022-05-11 19-32-31.png)
+
 * The second solution is that we can let as many as philosophers to eat but one philosopher can eat only when his left neighbors and right neighbors are not eating. Moreover, when a philosopher ends eating, he will notify his left and right neighbors and ask them if they want to eat.
+
+![Screenshot from 2022-05-11 19-47-02](/home/lrj11911808/Pictures/Screenshot from 2022-05-11 19-47-02.png)
+
+![Screenshot from 2022-05-11 19-47-11](/home/lrj11911808/Pictures/Screenshot from 2022-05-11 19-47-11.png)
+
+![Screenshot from 2022-05-11 19-47-21](/home/lrj11911808/Pictures/Screenshot from 2022-05-11 19-47-21.png)
+
+![Screenshot from 2022-05-11 19-49-07](/home/lrj11911808/Pictures/Screenshot from 2022-05-11 19-49-07.png)
 
 ## 3. The too much milk problem.
 
@@ -72,3 +85,14 @@ Design idea:
 * The second semaphore is named `fri_lock` which is used to indicate how many bottles of milk are currently in the fridge. Every time someone buys a bottle of milk, the `fri_lock` will be posted while son will first wait for a `fri_lock` semaphore before he opens the fridge. 
 * The third semaphore is name `milk_lock` which is used to indicate how many bottles of milk should the buyers buy. Every time someone wants to buy a bottle of milk, he will first wait for a `milk_lock` semaphore. Also, whenever son fetches a bottle a milk, he will post a `milk_lock` semaphore.
 
+![Screenshot from 2022-05-11 20-39-19](/home/lrj11911808/Pictures/Screenshot from 2022-05-11 20-39-19.png)
+
+![Screenshot from 2022-05-11 20-39-30](/home/lrj11911808/Pictures/Screenshot from 2022-05-11 20-39-30.png)
+
+![Screenshot from 2022-05-11 20-39-39](/home/lrj11911808/Pictures/Screenshot from 2022-05-11 20-39-39.png)
+
+![Screenshot from 2022-05-11 20-39-49](/home/lrj11911808/Pictures/Screenshot from 2022-05-11 20-39-49.png)
+
+![Screenshot from 2022-05-11 20-39-56](/home/lrj11911808/Pictures/Screenshot from 2022-05-11 20-39-56.png)
+
+![Screenshot from 2022-05-11 20-40-13](/home/lrj11911808/Pictures/Screenshot from 2022-05-11 20-40-13.png)
